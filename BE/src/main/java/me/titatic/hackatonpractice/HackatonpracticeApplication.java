065afbb43f5713dae37bdec238c8c56eef679c73
HYUNJUN <guswns1659@gmail.com>
2020-07-09 14:17:46 +0900

    Initial commit

---
+++ BE/src/main/java/me/titatic/hackatonpractice/HackatonpracticeApplication.java
@@ -0,0 +1,13 @@
+package me.titatic.hackatonpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonpracticeApplication.class, args);
+    }
+
+}
---
+++ BE/src/test/java/me/titatic/hackatonpractice/HackatonpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.titatic.hackatonpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
